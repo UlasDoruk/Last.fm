ebe2f549b42f31ede0892af4b3742d2483c33484
UlaşDK <udk97@hotmail.com>
2022-11-29 01:37:59 +0300

    NavBar and Footer added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import HomePage from './components/HomePage/HomePage';
 
 function App() {
   return (
-    <div>Hello</div>
+    <div>
+      <HomePage/>
+    </div>
   );
 }
 
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "../NavBar/NavBar.css"
+
+function NavBar() {
+  return (
+    <nav class="navbar navbar-dark bg-primary">
+      <div class="container-fluid">
+        <a class="navbar-brand">Last.FM</a>
+      </div>
+    </nav>
+  );
+}
+
+export default NavBar
